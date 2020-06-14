c57044dd31b9cea98c5642d5a3e184d20d66e68c
Ewan <chl8263@naver.com>
2020-06-13 23:27:54 -0700

    real init

--- lecture2/client.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import { hot } from 'react-hot-loader/root';
-
-import BaseBall from'./BaseBall';
-
-const Hot = hot(BaseBall);
-
-ReactDom.render(<Hot />, document.querySelector('#root'));
--- lecture6/client.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import { hot } from 'react-hot-loader/root';
-
-import Games from'./Games';
-
-const Hot = hot(Games);
-
-ReactDom.render(<Hot />, document.querySelector('#root'));
