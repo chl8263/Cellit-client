6444aa670b3ec5eff670dd837da41d763d96cd41
Ewan <chl8263@naver.com>
2020-09-07 22:17:14 -0700

    Create Caledndar, Charts, Gallery at side menu

--- src/component/cellUnit/ContentMatcher.jsx
+++ src/component/cellUnit/ContentMatcher.jsx
@@ -5,7 +5,11 @@ import SettingContainer from "./content/setting/SettingContainer";
 
 const ContentMatcher = (props) => {
 
-    if(props.match.params.name === "dashboard"){
+    if(props.match.params.name === "calendar"){
+        return (<div>calendar</div>);
+    }else if(props.match.params.name === "charts"){
+        return (<div>dash board</div>);
+    }else if(props.match.params.name === "gallery"){
         return (<div>dash board</div>);
     }else if(props.match.params.name === "channel"){
         return (<Channel data={props}/>);
